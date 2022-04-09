edf62d7dcc3490d907c7066c89f60a164851b672
HGJ <1825762870@qq.com>
2022-04-10 00:05:52 +0800

    commit

---
+++ src/test/java/GeneratorConfig.java
@@ -0,0 +1,28 @@
+import com.atguigu.mybatis.mapper.EmpMapper;
+import com.atguigu.mybatis.pojo.Emp;
+import com.atguigu.mybatis.pojo.EmpExample;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class GeneratorConfig {
+    @Test
+    public void generatorTest() throws IOException {
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSession sqlSession = new SqlSessionFactoryBuilder().build(is).openSession(true);
+        EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+        EmpExample empExample = new EmpExample();
+        empExample.createCriteria().andENameLike("张").andDidEqualTo(3);
+        empExample.or().andDidEqualTo(2);
+        List<Emp> emps = mapper.selectByExample(empExample);
+       emps.forEach(emp -> System.out.println(emp));
+
+    }
+
+
+}
